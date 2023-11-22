43b9e32088d911caaa9742d5cb4aaef2d9f756d8
Constanzamw <cweiner92@gmail.com>
2023-11-22 17:17:03 -0300

    2

--- connect-ink/src/app/page.js
+++ connect-ink/src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div style={{ position: "fixed", top: 0, left: 0, width: "100%", height: "100%", overflow: "hidden" }}>
       <Image
       src="/tat4.jpg"
-      alt="tat3"
+      alt="tatooback"
       className="dark:invert"
       layout="fill" 
       objectFit="cover" 
